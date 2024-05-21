75dd060c0113fec03f1a0a7f5ba7a88f928d2733
Danilo Faccio <39106747+DFaccio@users.noreply.github.com>
2024-05-21 00:44:40 -0300

    ajustando configurações para feign client (#17)
    ajustando configurações para feign client

--- src/main/java/br/com/productmanagement/ProductManagementApplication.java
+++ src/main/java/br/com/productmanagement/ProductManagementApplication.java
@@ -2,12 +2,14 @@ package br.com.productmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ProductManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/productmanagement/util/config/documentation/SwaggerConfig.java
+++ src/main/java/br/com/productmanagement/util/config/documentation/SwaggerConfig.java
@@ -2,6 +2,7 @@ package br.com.productmanagement.util.config.documentation;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ public class SwaggerConfig {
                                 .title("Microsserviço para controle de estoque de produtos")
                                 .description("APIs para cadastro e atualização de produtos, descontos e reservas")
                                 .version("1.0.0")
-                );
+                ).addServersItem(new Server().url("http://localhost:7071/order-management-system/product-microservice"))
+                .addServersItem(new Server().url("http://localhost:7077"));
     }
 }
